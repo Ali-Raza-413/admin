acd4aa10d0a2d88115fd4c7c64eda9cbeed7a685
Ali-Raza-413 <razachali89@gmail.com>
2024-12-17 18:08:25 +0500

    fixed issue

--- src/app/components/SIdebar.jsx
+++ src/app/components/SIdebar.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiOutlineDown, AiOutlineUp, AiOutlineMenu } from "react-icons/ai";
 import Image from "next/image";
